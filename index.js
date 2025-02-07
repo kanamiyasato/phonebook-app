956ae258a56db15e2a98ec6c1f4f84c60688244e
Kana <miyasatoka@gmail.com>
2025-02-07 12:49:48 -0500

    updated index.js

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -61,7 +63,7 @@ app.use(
   )
 );
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   const newName = body.name;
   const newNumber = body.number;
@@ -77,25 +79,35 @@ app.post("/api/persons", (request, response) => {
     number: newNumber,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
-});
-
-app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
-
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then((updatedPerson) => {
-      response.json(updatedPerson);
+  person.save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
     })
     .catch((error) => next(error));
 });
+
+app.put("/api/persons/:id", async (request, response, next) => {
+  try {
+    const { name, number } = request.body;
+    
+    const personFound = await Person.findById(request.params.id);
+    
+    if (!personFound) {
+      return response.status(400).json({ error: "Entry no longer exists in database" });
+    }
+
+    const updatedPerson = await Person.findByIdAndUpdate(
+      request.params.id,
+      { name, number },
+      { new: true, runValidators: true, context: "query" }
+    );
+
+    response.json(updatedPerson);
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 app.delete("/api/persons/:id", (request, response) => {
   Person.findByIdAndDelete(request.params.id)
